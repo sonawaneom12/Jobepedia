61a602aa2eda003c6363c3cf4eef00abfc4fd9d2
Om Sonawane <sonawaneom12@gmail.com>
2026-02-15 09:51:52 +0530

    This is a initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,85 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
+
+}
+
+android {
+    namespace = "com.jobepedia.app"
+    compileSdk {
+        version = release(36)
+        buildFeatures {
+            viewBinding = true
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.jobepedia.app"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    dependencies {
+
+        implementation("androidx.core:core-ktx:1.12.0")
+        implementation("androidx.appcompat:appcompat:1.6.1")
+
+        // Material UI
+        implementation("com.google.android.material:material:1.11.0")
+
+        // RecyclerView
+        implementation("androidx.recyclerview:recyclerview:1.3.2")
+
+        // Navigation
+        implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+        implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
+        // ViewModel + LiveData
+        implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+        implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+
+        // Firebase
+        implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+        implementation("com.google.firebase:firebase-firestore-ktx")
+        implementation("com.google.firebase:firebase-auth-ktx")
+
+        // Glide
+        implementation("com.github.bumptech.glide:glide:4.16.0")
+        kapt("com.github.bumptech.glide:compiler:4.16.0")
+    }
+
+
+}
