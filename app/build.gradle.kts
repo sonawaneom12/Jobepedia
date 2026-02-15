a3166968f3265491602884bebeddcb34bff9b85f
Om Sonawane <sonawaneom12@gmail.com>
2026-02-15 13:05:27 +0530

    Important Firebase Integration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     id("kotlin-kapt")
+    id("com.google.gms.google-services")
 
 }
 
@@ -79,6 +80,14 @@ dependencies {
         // Glide
         implementation("com.github.bumptech.glide:glide:4.16.0")
         kapt("com.github.bumptech.glide:compiler:4.16.0")
+
+        // Firebase BOM (VERY IMPORTANT — gives versions automatically)
+        implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+
+        // Firebase libraries (NO version here)
+        implementation("com.google.firebase:firebase-firestore-ktx")
+        implementation("com.google.firebase:firebase-auth-ktx")
+
     }
 
 
